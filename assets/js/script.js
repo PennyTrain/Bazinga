5b2c5162a10ec2317c88afba84d658ffe275ad56
Penny Train <penny.train04@gmail.com>
2023-08-25 10:55:09 +0100

    Add JS, HTML content and Styles
    Fixed bug button issue, making it hide the start box and display the game box,

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,32 @@
+// Labeling of the start of the game and getting the element
 let gameStartButton = document.getElementById("play");
-
 gameStartButton.addEventListener('click', playGameModal);
+const gameBoxModal = document.getElementById("game-box");
+const startModal = document.getElementById("start");
+
+// Rules modal and event listeners
+const rulesModal = document.getElementById("rules");
+const rulesButton = document.getElementById("rulesButton");
+console.log(rulesButton)
+rulesButton.addEventListener('click', showRules);
+
+// Starts the game modal
 function playGameModal() {
   console.log("IN play game")
-  startModal = document.getElementById("start");
   startModal.classList.toggle("hidden")
-  let gameBoxModal = document.getElementById("game-box")
   gameBoxModal.classList.toggle("hidden")
+}
+
+function showRules() {
+  const mainMenuButton = document.getElementById('back-button');
+  console.log("showing rules!:D")
+  startModal.classList.toggle("hidden")
+  rulesModal.classList.toggle("hidden")
+  mainMenuButton.addEventListener('click', mainMenu)
+}
+
+function mainMenu() {
+  window.location.reload();
 }
 
 const resultMatch = document.querySelector('#match-result');
@@ -19,11 +39,13 @@ let player;
 let computer;
 
 allButtons.forEach(button => button.addEventListener('click', function() {
-  player = button.textContent;
+  console.log(button.getAttribute('data-value'))
+  player = button.getAttribute('data-value')
+  console.log(player)
   computerChoice();
-  playerScore.textContent = `Player: ${player}`;
+  playerScore.textContent  = `Player: ${player}`;
   computerScore.textContent = `Computer: ${computer}`;
-  resultMatch.textContent = checkWin();
+  resultMatch.textContent = checkWin(player, computer);
 }))
 
 function computerChoice() {
@@ -48,17 +70,67 @@ function computerChoice() {
   }
 }
 
-function checkWin(){
-  if(player == computer){
-  return "Draw!";
-  } else if (computer == "rock"){
-    return (player = "paper") ? "You Win!" : "You Lose!"
+function checkWin(player, computer) {
+  console.log(player)
+  console.log(computer)
+  if (player === computer) {
+    return "Draw!";
+  } else if (computer === "rock") {
+    if (player === "paper" || player === "spock") {
+      win()
+      return "You Win!";
+    } else {
+      lose()
+      return "You Lose!";
+    }
+  } else if (computer === "paper") {
+    if (player === "scissors" || player === "lizard") {
+      win()
+      return "You Win!";
+    } else {
+      lose()
+      return "You Lose!";
+    }
+  } else if (computer === "scissors") {
+    if (player === "rock" || player === "spock") {
+      win()
+      return "You Win!";
+    } else {
+      lose()
+      return "You Lose!";
+    }
+  } else if (computer === "lizard") {
+    if (player === "rock" || player === "scissors") {
+      win()
+      return "You Win!";
+    } else {
+      lose()
+      return "You Lose!";
+    }
+  } else if (computer === "spock") {
+    if (player === "paper" || player === "lizard") {
+      win()
+      return "You Win!";
+    } else {
+      lose()
+      return "You Lose!";
+    }
   }
-  else if (computer == "paper"){
-    return (player = "scissors") ? "You Win!" : "You Lose!"
-  }
-  else if (computer == "scissors"){
-    return (player = "rock") ? "You Win!" : "You Lose!"
-  }
+}
+
+let aiScoreBoard = document.getElementById("ai-score");
+let aiScore = 0;
+let userScoreBoard = document.getElementById("user-score");
+let userScore = 0;
+
+function win() {
+  userScore++
+  userScoreBoard.innerHTML = userScore;
+}
+
+
+function lose(){
+  aiScore++ 
+  aiScoreBoard.innerHTML = aiScore;
 }
 
