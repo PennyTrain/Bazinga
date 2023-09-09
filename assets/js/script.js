18cf387a950460eb5ca0946d6e4d531596896cb2
Penny Train <penny.train04@gmail.com>
2023-09-09 17:14:20 +0100

    Edit javascript for final touches and added comments throughout JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ gameStartButton.addEventListener('click', playGameModal);
 const gameBoxModal = document.getElementById("game-box");
 const startModal = document.getElementById("homepage");
 
-// Check if the night mode is set in sessionStorage
+// Check if the night mode is set in localStorage
 let nightMode = localStorage.getItem("nightMode") === "True";
 
 // Function to set night mode state and update background image
@@ -40,6 +40,7 @@ window.addEventListener("load", () => {
   setNightMode(nightMode);
 });
 
+// Get all the back buttons and add event listener
 let allBackButtons = document.querySelectorAll('.back-button');
 allBackButtons.forEach(backButton => {
   backButton.addEventListener('click', mainMenu);
@@ -55,6 +56,7 @@ const settingBoxModal = document.getElementById("settings-box");
 const settingsButton = document.getElementById("settingsButton");
 settingsButton.addEventListener('click', showSettings);
 
+// Display the settings
 function showSettings() {
   startModal.classList.toggle("hidden");
   if (localStorage.gameAmount == 3) {
@@ -71,23 +73,26 @@ function playGameModal() {
   gameBoxModal.classList.toggle("hidden");
 }
 
+// Displays the rulesModal and hides startModal
 function showRules() {
   startModal.classList.toggle("hidden");
   rulesModal.classList.toggle("hidden");
 }
 
+// Enables the retry button to go to the main menu
 let retryButton = document.getElementById("retry");
 retryButton.addEventListener('click', mainMenu);
 
+// Displays the main menu by refreshing the page 
 function mainMenu() {
   location.reload();
 }
 
+// Declaring variables for the game logic
 const resultMatch = document.querySelector('#match-result');
 const playerScore = document.querySelector('#player-score');
 const computerScore = document.querySelector('#computer-score');
 let allButtons = document.querySelectorAll('.game-buttons');
-
 let result;
 let player;
 let computer;
@@ -95,6 +100,7 @@ let roundsPlayed = 0;
 let matchAmount = 0;
 let gameAmount = 0;
 
+// Checking the amount of games
 let matchToggle = document.getElementById("match-amount-toggle");
 matchToggle.addEventListener('change', amountOfGames);
 function amountOfGames(matchAmount) {
@@ -107,23 +113,28 @@ function amountOfGames(matchAmount) {
   }
 }
 
-
+// Adding event listeners
 allButtons.forEach(button => button.addEventListener('click', gameLogic));
 
+// Main game logic
 function gameLogic(event) {
+  allButtons.forEach(button => button.removeEventListener("click", gameLogic));
+  // Setting the match amount
   if (localStorage.getItem("gameAmount") == 3) {
     matchAmount = 3;
   } else {
     matchAmount = 5;
   }
-  //how im assigning game amount in logic
-  //only need 4, stay w match amount variable, as default is 2 automatically 
+
+// Checking if the rounds played is less than the match amount
   if (roundsPlayed <= matchAmount) {
     button = event.target;
     player = button.getAttribute('data-value');
     computerChoice();
+    // Adding player and computer text content
     playerScore.textContent = `Player: ${player}`;
     computerScore.textContent = `Computer: ${computer}`;
+    // Checking the round result
     resultMatch.textContent = checkWin(player, computer);
     if (resultMatch.textContent == "You Win!") {
       resultMatch.style.color = "green";
@@ -132,18 +143,27 @@ function gameLogic(event) {
     } else if (resultMatch.textContent == "You Lose!") {
       resultMatch.style.color = "red";
     }
-    if (roundsPlayed === matchAmount) {
-      // Display game over message or perform any necessary actions
-      alert("Selected amount of rounds completed");
-      setTimeout(() => {
-        endGameResults();
-      }, 2000); // 2000 milliseconds = 2 seconds
-    }
+  }
+
+  // Checking if rounds played is equal to the match amount
+  if (roundsPlayed === matchAmount) {
+    // Display game over message or perform any necessary actions
+    alert("Selected amount of rounds completed");
+    setTimeout(() => {
+      endGameResults();
+    }, 2000); // 2000 milliseconds = 2 seconds
+  } else {
+      // Remove the highlighting after 2 seconds
+    setTimeout(() => {
+      let computerChoiceButton = document.querySelector(`[data-value="${computer}"]`);
+      computerChoiceButton.classList.remove('highlight-computer-choice');
+      allButtons.forEach(button => button.addEventListener('click', gameLogic));
+    }, 2000); // 2000 milliseconds = 2 seconds
   }
 };
 
+// Assigning the computers choice
 function computerChoice() {
-  allButtons.forEach(button => button.removeEventListener("click", computerChoice));
   roundsPlayed++;
   const randNum = Math.floor(Math.random() * 5) + 1;
   switch (randNum) {
@@ -163,16 +183,12 @@ function computerChoice() {
       computer = "spock";
       break;
   }
-
+  // Adding css affect to the computers choice
   const computerChoiceButton = document.querySelector(`[data-value="${computer}"]`);
   computerChoiceButton.classList.add('highlight-computer-choice');
-
-  // Remove the highlighting after 2 seconds
-  setTimeout(() => {
-    computerChoiceButton.classList.remove('highlight-computer-choice');
-  }, 2000); // 2000 milliseconds = 2 seconds
 }
 
+// Calculating who won the round
 function checkWin(player, computer) {
   if (player === computer) {
     return "Draw !";
@@ -219,23 +235,27 @@ function checkWin(player, computer) {
   }
 }
 
+// Declarinf variables for the score board
 let aiScoreBoard = document.getElementById("ai-score");
 let aiScore = 0;
 let userScoreBoard = document.getElementById("user-score");
 let userScore = 0;
 
+// Displaying text if player won
 function win() {
   userScore++;
   userScoreBoard.innerHTML = userScore;
 }
 
+// Displaying text if player lost
 function lose() {
   aiScore++;
   aiScoreBoard.innerHTML = aiScore;
 }
 
-const endGameBoxModal = document.getElementById("game-end");
+// Displaying the end 
 function endGameResults() {
+  const endGameBoxModal = document.getElementById("game-end");
   gameBoxModal.classList.toggle("hidden");
   endGameBoxModal.classList.toggle("hidden");
   let finalComputerScore = document.getElementById("final-computer-score");
@@ -243,10 +263,13 @@ function endGameResults() {
   finalPlayerScore.textContent = `${userScore}`;
   finalComputerScore.textContent = `${aiScore}`;
   let finalMessageWin = 'Congrats you won! Smashed It';
+  let finalMessageDraw = 'It was a draw!';
   let finalMessageLoss = 'Oh no! You lost';
   let finalMessageContainer = document.getElementById('final-game-messsage');
   if (aiScore > userScore) {
     finalMessageContainer.innerText = finalMessageLoss;
+  } else if (aiScore === userScore){
+    finalMessageContainer.innerText = finalMessageDraw;
   } else {
     finalMessageContainer.innerText = finalMessageWin;
   }
